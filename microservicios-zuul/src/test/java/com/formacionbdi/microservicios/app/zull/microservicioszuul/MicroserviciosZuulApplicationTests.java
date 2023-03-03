bd591011099487f437a326ef10eceb6dbb4f2187
Antonio Libreros Lopez <librerosfcfm@gmail.com>
2023-03-03 11:26:02 -0600

    microservicios-zuul iniciado

---
+++ microservicios-zuul/src/test/java/com/formacionbdi/microservicios/app/zull/microservicioszuul/MicroserviciosZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.microservicios.app.zull.microservicioszuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
